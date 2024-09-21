6f2c02f894ab9a131eb87094fe96c2d703e086e5
fabiooo4 <fabioirimie@gmail.com>
2024-09-21 13:51:15 +0200

    refactored main

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,17 @@ use app::CurrentScreen;
 use crossterm::event::{self, Event, KeyCode};
 use ratatui::DefaultTerminal;
 use simulations::ant::AntSim;
-use std::io;
+use std::io::{self};
 use std::time::Duration;
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
+
     let mut app = App::new();
     let app_result = run_app(&mut terminal, &mut app);
+
     ratatui::restore();
+
     app_result
 }
 
@@ -25,44 +28,6 @@ fn run_app(terminal: &mut DefaultTerminal, app: &mut App) -> io::Result<()> {
         terminal.draw(|f| ui(f, app))?;
 
         // Event handling
-        // Only run when a key is pressed
-        if is_event_available(app.speed)? || !app.is_running {
-            if let Event::Key(key) = event::read()? {
-                if key.kind == event::KeyEventKind::Release {
-                    // Skip events that are not KeyEventKind::Press
-                    continue;
-                }
-
-                if app.help_screen {
-                    // Prevent any action when the help screen is displayed
-                    app.help_screen = false;
-                    app.is_running = true;
-                } else {
-                    match app.current_screen {
-                        CurrentScreen::Ant => match key.code {
-                            KeyCode::Char('q') => app.current_screen = CurrentScreen::Exit,
-                            KeyCode::Char(' ') => app.is_running = !app.is_running,
-                            KeyCode::Char('?') => {
-                                app.help_screen = !app.help_screen;
-                                app.is_running = false;
-                            }
-                            KeyCode::Right | KeyCode::Char('l') | KeyCode::Char('L') => {
-                                AntSim::run_ant_sim(app);
-                            }
-                            KeyCode::Up | KeyCode::Char('j') | KeyCode::Char('J') => {
-                                app.speed = app.speed.saturating_sub(Duration::from_millis(10));
-                            }
-                            KeyCode::Down | KeyCode::Char('k') | KeyCode::Char('K') => {
-                                app.speed = app.speed.saturating_add(Duration::from_millis(10));
-                            }
-                            _ => {}
-                        },
-                        _ => {}
-                    }
-                }
-            }
-        }
-
         match app.current_screen {
             CurrentScreen::Exit => break Ok(()),
             CurrentScreen::Ant => {
@@ -72,6 +37,46 @@ fn run_app(terminal: &mut DefaultTerminal, app: &mut App) -> io::Result<()> {
                 }
             }
             _ => {}
+        }
+
+        // Only run when a key is pressed
+        if !is_event_available(app.speed)? {
+            continue;
+        }
+
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Release {
+                // Skip events that are not KeyEventKind::Press
+                continue;
+            }
+
+            if app.help_screen {
+                // Prevent any action when the help screen is displayed
+                app.help_screen = false;
+                app.is_running = true;
+            } else {
+                match app.current_screen {
+                    CurrentScreen::Ant => match key.code {
+                        KeyCode::Char('q') => app.current_screen = CurrentScreen::Exit,
+                        KeyCode::Char(' ') => app.is_running = !app.is_running,
+                        KeyCode::Char('?') => {
+                            app.help_screen = !app.help_screen;
+                            app.is_running = false;
+                        }
+                        KeyCode::Right | KeyCode::Char('l') | KeyCode::Char('L') => {
+                            AntSim::run_ant_sim(app);
+                        }
+                        KeyCode::Up | KeyCode::Char('j') | KeyCode::Char('J') => {
+                            app.speed = app.speed.saturating_sub(Duration::from_millis(10));
+                        }
+                        KeyCode::Down | KeyCode::Char('k') | KeyCode::Char('K') => {
+                            app.speed = app.speed.saturating_add(Duration::from_millis(10));
+                        }
+                        _ => {}
+                    },
+                    _ => {}
+                }
+            }
         }
     }
 }
